b38c0a5c372080ce6149e20aff1f7ba43a22d608
Nikola Bukvic <117097070+nikola017@users.noreply.github.com>
2023-10-29 11:17:52 +0100

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 router.get('/home', (req, res) => {
     if (req.oidc.isAuthenticated()) {
+        console.log(req.oidc.user);
         res.render('home', { user: req.oidc.user });  // Ako je korisnik autenticiran, prikažite mu home stranicu.
     } else {
         res.redirect('/');  // Ako korisnik nije autenticiran, preusmjerite ga natrag na glavnu stranicu.
