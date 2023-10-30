47da61a34eac4f50426549a7a397fd0ccf02e2e6
Nikola Bukvic <117097070+nikola017@users.noreply.github.com>
2023-10-30 03:38:30 +0100

    Dodan prikaz

--- routes/home.js
+++ routes/home.js
@@ -101,7 +101,8 @@ router.post('/createTournament', async (req, res) => {
             `, [tournamentId]);
         
             const competitorIds = competitorsResult.rows.map(row => row.id);
-            
+
+            let roundNumber=1
             // Ovdje generiramo raspored; za sada pretpostavimo da svaki natjecatelj igra s svakim
             for (let i = 0; i < competitorIds.length; i++) {
                 for (let j = i + 1; j < competitorIds.length; j++) {
@@ -154,9 +155,61 @@ router.post('/submitResults', async (req, res) => {
     
     for (const [resultId, scores] of Object.entries(resultUpdates)) {
         await pool.query(`UPDATE Results SET score1 = $1, score2 = $2 WHERE id = $3`, [scores.score1, scores.score2, resultId]);
+        
+        // Fetch scoring system
+        const tournamentIdQuery = await pool.query(`SELECT tournament_id FROM Results WHERE id = $1`, [resultId]);
+        const tournamentId = tournamentIdQuery.rows[0].tournament_id;
+        const scoringSystemQuery = await pool.query(`SELECT scoring_system FROM Tournaments WHERE id = $1`, [tournamentId]);
+        const [winPoints, drawPoints, losePoints] = scoringSystemQuery.rows[0].scoring_system.split('/').map(Number);
+        
+        // Ažuriranje bodova natjecatelja
+        const [score1, score2] = [Number(scores.score1), Number(scores.score2)];
+        const result = await pool.query(`SELECT competitor1_id, competitor2_id FROM Results WHERE id = $1`, [resultId]);
+        const {competitor1_id, competitor2_id} = result.rows[0];
+        
+        if (score1 > score2) {
+            await pool.query(`UPDATE Competitors SET total_points = total_points + $1 WHERE id = $2`, [winPoints, competitor1_id]);
+            await pool.query(`UPDATE Competitors SET total_points = total_points + $1 WHERE id = $2`, [losePoints, competitor2_id]);
+        } else if (score1 < score2) {
+            await pool.query(`UPDATE Competitors SET total_points = total_points + $1 WHERE id = $2`, [losePoints, competitor1_id]);
+            await pool.query(`UPDATE Competitors SET total_points = total_points + $1 WHERE id = $2`, [winPoints, competitor2_id]);
+        } else {
+            await pool.query(`UPDATE Competitors SET total_points = total_points + $1 WHERE id = $2`, [drawPoints, competitor1_id]);
+            await pool.query(`UPDATE Competitors SET total_points = total_points + $1 WHERE id = $2`, [drawPoints, competitor2_id]);
+        }
     }
     
     res.redirect('/home');
 });
+
+
+
+router.get('/tournamentDetails/:tournamentId', async (req, res) => {
+    const { tournamentId } = req.params;
+
+    // Dohvati turnir po ID-u
+    const tournament = await pool.query(`SELECT * FROM Tournaments WHERE id = $1`, [tournamentId]);
+
+    // Dohvati rezultate za turnir
+    const results = await pool.query(`
+        SELECT r.*, c1.name as competitor1_name, c2.name as competitor2_name 
+        FROM Results r
+        INNER JOIN Competitors c1 ON r.competitor1_id = c1.id
+        INNER JOIN Competitors c2 ON r.competitor2_id = c2.id
+        WHERE r.tournament_id = $1
+    `, [tournamentId]);
+
+    // Dohvati natjecatelje za turnir poredane po bodovima
+    const competitors = await pool.query(`
+        SELECT * FROM Competitors WHERE tournament_id = $1 ORDER BY total_points DESC
+    `, [tournamentId]);
+
+    res.render('tournamentDetails', { 
+        tournament: tournament.rows[0],
+        results: results.rows, 
+        competitors: competitors.rows 
+    });
+});
+
 
 module.exports = router;
